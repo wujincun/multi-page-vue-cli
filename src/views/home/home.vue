d1079f65cd81db4d05251d307d96c07548021106
wujincun <wujincun@guazi.com>
2018-10-20 16:46:40 +0800

    多页面应用初始配置

---
+++ src/views/home/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="home">home</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+
+</script>
+<style lang="less" rel="stylesheet/less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  .home {
+    height: 500px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+</style>
